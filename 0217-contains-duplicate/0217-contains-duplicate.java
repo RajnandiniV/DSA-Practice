41c3db661209a4ba402e7a74060ee8096b6cb3ba
Rajnandini Verma <rajnandini.v5@gmail.com>
2026-07-02 23:07:14 +0530

    Time: 18 ms (36.49%), Space: 108.1 MB (5.12%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> ans=new HashSet<>();
+
+        for(int i =0;i<nums.length;i++){
+            if(ans.contains(nums[i])){
+                return true;
+            }else{
+                ans.add(nums[i]);
+            }
+        }
+        return false;
+
+    }
+}
